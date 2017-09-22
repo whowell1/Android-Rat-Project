82f1e447defd8ea80dca4a53048bb5c05813b881
wren <wren@lawn-128-61-126-116.lawn.gatech.edu>
2017-09-22 13:44:04 -0400

    M4 Project initial project

---
+++ RatApplicationsTest/app/src/main/java/cs2340/ratapplicationstest/LoginSuccess.java
@@ -0,0 +1,28 @@
+package cs2340.ratapplicationstest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginSuccess extends AppCompatActivity {
+    private Button Logout;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Logout = (Button) findViewById(R.id.logoutBtn);
+        Logout.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginSuccess.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ RatApplicationsTest/app/src/main/java/cs2340/ratapplicationstest/MainActivity.java
@@ -0,0 +1,80 @@
+package cs2340.ratapplicationstest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password= (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button) findViewById(R.id.loginBtn);
+
+        Info.setText("No of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(),Password.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPassword) {
+
+        if ((userName.equals("user")) && (userPassword.equals("pass"))){
+            Intent intent = new Intent(MainActivity.this, LoginSuccess.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this,"Improper Login", Toast.LENGTH_LONG).show();
+            counter --;
+            Info.setText("No attempts left: " + String.valueOf(counter));
+            if (counter == 0) {
+                Login.setEnabled(false);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
