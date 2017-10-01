a6be2e8aa3e24ab6084480d0861fa2699bbd1581
Thomas Olson <tolson9@gatech.edu>
2017-10-01 11:30:50 -0400

    Added SignUp page. Does not remember new  accounts yet

--- RatApplicationsTest/app/src/main/java/cs2340/ratapplicationstest/SignupPage.java
+++ RatApplicationsTest/app/src/main/java/cs2340/ratapplicationstest/SignupPage.java
@@ -3,15 +3,19 @@ package cs2340.ratapplicationstest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by thoma on 9/29/2017.
+ */
+
+public class SignupPage extends AppCompatActivity {
 
     //These are the fields that we needed intialized here.
     // username, password, Info is how many attempts we have left
@@ -19,39 +23,21 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText Name;
     private EditText Password;
-    private TextView Info;
-    private Button Login;
     private Button Signup;
-    private int counter = 0;
     @Override
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
         Name = (EditText)findViewById(R.id.etName);
         Password= (EditText)findViewById(R.id.etPassword);
-        Info = (TextView)findViewById(R.id.tvInfo);
-        Login = (Button) findViewById(R.id.loginBtn);
-
-        Info.setText("No of attempts remaining: 3 ");
-
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                validate(Name.getText().toString(),Password.getText().toString());
-            }
-        });
-
         Signup = (Button) findViewById(R.id.signupBtn);
-
-        Info.setText("No of attempts remaining: 3 ");
 
         Signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SignupPage.class);
-                startActivity(intent);
+                validate(Name.getText().toString(),Password.getText().toString());
             }
         });
     }
@@ -65,16 +51,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void validate(String userName, String userPassword) {
 
-        if ((userName.equals("user")) && (userPassword.equals("pass"))){
-            Intent intent = new Intent(MainActivity.this, LoginSuccess.class);
+        if ((userName.contains("@") && userPassword.length() >= 7)){
+            Intent intent = new Intent(SignupPage.this, LoginSuccess.class);
             startActivity(intent);
+        } else  if(!userName.contains("@")) {
+            Toast.makeText(this,"Must enter a valid email", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(this,"Improper Login", Toast.LENGTH_LONG).show();
-            counter --;
-            Info.setText("No attempts left: " + String.valueOf(counter));
-            if (counter == 0) {
-                Login.setEnabled(false);
-            }
+            Toast.makeText(this,"Password must be longer than 7", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -100,3 +83,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
